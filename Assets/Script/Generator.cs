91ddaeff834e2c93ac3e70b38b9bc812f149564f
Denaton <dennisMagnusson_4@hotmail.com>
2015-06-14 01:17:16 +0200

    Update

--- Assets/Script/Generator.cs
+++ Assets/Script/Generator.cs
@@ -4,8 +4,11 @@ using System.Collections;
 public class Generator : MonoBehaviour {
 	public Transform Tiles;
 	public Vector2 size;
+
+	Camera camera;
 	// Use this for initialization
 	void Start () {
+		camera = GetComponent<Camera>();
 		for (int x = 0; x < size.x; x++) {
 			for (int y = 0; y < size.y; y++) {
 				Instantiate(Tiles, new Vector3(x,y,0), Quaternion.identity);
@@ -23,11 +26,12 @@ public class Generator : MonoBehaviour {
 		GUI.skin = skin;
 		GameObject[] players = GameObject.FindGameObjectsWithTag("Unit");
 		for (int i = 0; i < players.Length; i++) {
-			GUI.Box(new Rect((Screen.width-200), 40*i, 200, 40*i), "","HealthBar");
-			GUI.BeginGroup(new Rect((Screen.width-200),40*i, 200*(players[i].GetComponent<Stats>().currentLife/players[i].GetComponent<Stats>().maxLife), 40));
-			GUI.Box(new Rect(0, 0, 200, 40), "","HealthBarBackground");
+			Vector3 screenPos = camera.WorldToScreenPoint(players[i].transform.position);
+			GUI.Box(new Rect(screenPos.x-32, Screen.height - screenPos.y - 64, 64, 64), "","HealthBar");
+			GUI.BeginGroup(new Rect(screenPos.x-32, Screen.height - screenPos.y - 64, 64*(players[i].GetComponent<Stats>().currentLife/players[i].GetComponent<Stats>().maxLife), 64));
+			GUI.Box(new Rect(0, 0, 64, 64), "","HealthBarBackground");
 			GUI.EndGroup();
-			GUI.Label (new Rect ((Screen.width-200), 40*i, 200, 40), Mathf.Floor((players[i].GetComponent<Stats>().currentLife/players[i].GetComponent<Stats>().maxLife)*100).ToString() + "%","LabelCenter");
+			GUI.Label (new Rect (screenPos.x-32, Screen.height - screenPos.y - 64, 64, 64), Mathf.Floor((players[i].GetComponent<Stats>().currentLife/players[i].GetComponent<Stats>().maxLife)*100).ToString() + "%","LabelCenter");
 		}
 	}
 }
