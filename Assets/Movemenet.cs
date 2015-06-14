7659b9fe9cfae438dc4d08c6763c03beae0dae40
kevin <kevinferm@gmail.com>
2015-06-14 10:12:29 +0200

    fix movement

--- Assets/Movemenet.cs
+++ Assets/Movemenet.cs
@@ -5,31 +5,77 @@ public class Movemenet : MonoBehaviour {
 
 	int MovementSpeed = 10;
 	private Gyroscope gyro;
+	public bool canMove = false;
+	public float time = 0f;
+	public bool cooldown = false;
+	float currentTimer = 0;
 	// Use this for initialization
 	void Start () {
+		Screen.autorotateToPortrait = true;
 		if (SystemInfo.supportsGyroscope) {
 			gyro = Input.gyro;
 			gyro.enabled = true;
 		}
 	}
+
+	void startTimer(){
+		currentTimer = time;
+	}
+
+	public bool countDown(){
+		if (currentTimer > 0) {
+			currentTimer -= Time.deltaTime;
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	void Move (int x, int y) {
+		if (canMove) {
+			Vector3 Movement = transform.position;
+			Movement.x += x;
+			Movement.y += y;
+			transform.position = Movement;
+			canMove = false;
+		} else if (countDown()) {
+			canMove = true;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
+		Screen.orientation = ScreenOrientation.Portrait;
 		// Movement
-		Vector3 Movement = transform.position;
-		if (Input.GetKeyUp(KeyCode.RightArrow)) {
-			Movement.x += 1;
+		//Vector3 Movement = transform.position;
+		Debug.Log (gyro.gravity);
+		if (countDown ()) {
+			canMove = true;
+			cooldown = false;
 		}
-		else if (Input.GetKeyUp(KeyCode.LeftArrow)) {
-			Movement.x -= 1;
+		if (canMove) {
+			if(!cooldown) {
+				startTimer();
+				cooldown = true;
+			}
 		}
-		else if (Input.GetKeyUp(KeyCode.UpArrow)) {
-			Movement.y += 1;
+		if (gyro.gravity.x >= 0.7) {
+			//Movement.x += 1;
+			Move (1,0);
 		}
-		else if (Input.GetKeyUp(KeyCode.DownArrow)) {
-			Movement.y -= 1;
+		else if (gyro.gravity.x <= -0.7) {
+			//Movement.x -= 1;
+			Move (-1,0);
 		}
-		transform.position = Movement;
+		else if (gyro.gravity.y >= 0.7) {
+			//Movement.y += 1;
+			Move (0,1);
+		}
+		else if (gyro.gravity.y <= -0.7) {
+			//Movement.y -= 1;
+			Move (0,-1);
+		}
+		//transform.position = Movement;
 
 	}
 
@@ -37,6 +83,7 @@ public class Movemenet : MonoBehaviour {
 	{
 		if (gyro!=null&&gyro.enabled) 
 		{	
+
 			Vector3 movement=new Vector3(gyro.gravity.x, 0.0f, gyro.gravity.y);
 
 			//spin (for later use)
